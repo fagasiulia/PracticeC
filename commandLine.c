18526e620e728c29804115c7a2a29c098d5b68ec
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-02-06 22:52:47 +0200

    Update commandLine.c

--- commandLine.c
+++ commandLine.c
@@ -3,16 +3,6 @@
 
 int main(int argc, char *argv [])
 {
-    char *argument1 = argv[0];
-    char *argument2 = argv[1];
-    char *argument3 = argv[2];
-    char *argument4 = argv[3];
-    char *argument5 = argv[4];
-    char *argument6 = argv[5];
-    char *argument7 = argv[6];
-    char *argument8 = argv[7];
-    char *argument9 = argv[8];
-    char *argument10 = argv[9];
     checkNumbers(argc);
     
 
