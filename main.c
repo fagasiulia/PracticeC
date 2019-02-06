de4c08d05f5ed1a3ac5b13cadf3919e61d5a7e33
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-02-06 22:38:34 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include "Functions.h"
+
+int main(int argc, char *argv [])
+{
+    char *argument1 = argv[0];
+    char *argument2 = argv[1];
+    char *argument3 = argv[2];
+    char *argument4 = argv[3];
+    char *argument5 = argv[4];
+    char *argument6 = argv[5];
+    char *argument7 = argv[6];
+    char *argument8 = argv[7];
+    char *argument9 = argv[8];
+    char *argument10 = argv[9];
+    checkNumbers(argv);
+    
+
+    return 0;
+}
