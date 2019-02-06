a28385d7f346524fef756dadcd6d62fa89dc862b
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-02-06 22:57:44 +0200

    Update commandLine.c

--- commandLine.c
+++ commandLine.c
@@ -3,6 +3,10 @@
 
 int main(int argc, char *argv [])
 {
+    printf("The command line has the following parameters:\n");
+    for(int i = 0; i < argc; i++){
+        printf("argv[%d] : %s \n", i , agrv[i]);
+    }
     checkNumbers(argc);
     
 
