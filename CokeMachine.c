57d8c3671adff7891f9eb34c85545bfc620a5e96
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-02-18 15:33:48 +0200

    Update CokeMachine.c

--- CokeMachine.c
+++ CokeMachine.c
@@ -3,12 +3,13 @@
 
 enum states {
     START,
+    ASK_FOR_MONEY,
     EXIT_LOOP
 }states;
 
 enum events{
-    ASK_FOR_MONEY,
     CHECK_MONEY,
+    NOT_ENOUGH_MONEY,
     DISPENSE_COKE,
     DISPENSE_CHANGE
 }event;
@@ -16,53 +17,52 @@ enum events{
 void step_state(int price){
     int n = price;
     int money = 0;
-    int moneyInserted;
+    int moneyInserted = 0;
     int state = START;
-    int event = ASK_FOR_MONEY;
+    int event = CHECK_MONEY;
     
-while (n != 0){
+    while (n != 0){
         switch(state){
             case START:
-                 switch(event){
-                       case ASK_FOR_MONEY:
-                            printf("Please insert money: \n");
-                            scanf("%d", &moneyInserted);
-                            money += moneyInserted;
-                            event = CHECK_MONEY;
-                       break;
-            
-                       case CHECK_MONEY:
-                            if(money >= n){
-                                printf("Enough money\n");
-                                event = DISPENSE_COKE;
-                            }          
-                            else {
-                                printf("Not enough money\n");
-                                event = ASK_FOR_MONEY;
-                            }
-                       break;
-            
-                       case DISPENSE_COKE:
-                            printf("Enjoy\n");
-                            if(money > n){
-                                 event = DISPENSE_CHANGE;
-                            }
-                            else{
-                                 state = EXIT_LOOP;
-                            }
-                       break;
-    
-                       case DISPENSE_CHANGE:
-                            printf("Your change is %d\n", money - n);
+                switch(event){
+                    case CHECK_MONEY: 
+                       if(money < n){
+                           state = ASK_FOR_MONEY;
+                       }
+                       else{
+                           event = DISPENSE_COKE;
+                       }
+                    break;
+                    
+                    case DISPENSE_COKE:
+                        printf("Enjoy\n");
+                        if(money > n){
+                            event = DISPENSE_CHANGE;
+                        }
+                        else{
                             state = EXIT_LOOP;
-                       break;
-                  }
+                        }
+                    break;
+                    
+                    case DISPENSE_CHANGE:
+                        printf("Your change is %d: \n", money - n);
+                        state = EXIT_LOOP;
+                    break;
+                }
+                break;
+                
+            case ASK_FOR_MONEY:
+                printf("Insert money: \n");
+                scanf("%d", &moneyInserted);
+                money += moneyInserted;
+                event = CHECK_MONEY;
             break;
             
             case EXIT_LOOP:
                 printf("Thank you!");
+                state = START; 
                 exit(1);
-    }
+        }
     
-}    
+    }    
 }
