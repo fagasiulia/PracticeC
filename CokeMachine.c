b23b5a62c20a629bec41714ba61de2fb91f6dec1
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-02-18 15:34:37 +0200

    Update CokeMachine.c

--- CokeMachine.c
+++ CokeMachine.c
@@ -61,7 +61,6 @@ void step_state(int price){
             case EXIT_LOOP:
                 printf("Thank you!");
                 state = START; 
-                exit(1);
         }
     
     }    
