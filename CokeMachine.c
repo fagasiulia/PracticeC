fabca7afea86655123598cd4e7c2e86a31d82857
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-02-21 11:52:03 +0200

    Update CokeMachine.c

--- CokeMachine.c
+++ CokeMachine.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 
 enum states {
-    ASK_FOR_MONEY,
+    START,
     DISPENCE_COKE,
     DISPENCE_CHANGE,
     EXIT_LOOP
 }states;
 
 enum events{
+    ASK_FOR_MONEY,
     CHECK_MONEY,
     NOT_ENOUGH_MONEY,
     ENOUGH_MONEY
@@ -19,56 +20,62 @@ void step_state(int price, int quantity){
 	int y = quantity;
 	int money = 0;
 	int moneyInserted = 0;
-	int state=ASK_FOR_MONEY;
-	int event=CHECK_MONEY;
-
+	int state=START;
+	int event=ASK_FOR_MONEY;
+	
     while(y > 0){
         switch(state){
-            case ASK_FOR_MONEY:
-                printf("Insert the money \n");
-                scanf("%d", &moneyInserted);
-                money += moneyInserted;
-                
+            case START:
                 switch(event){
+                    case ASK_FOR_MONEY:
+                        printf("Insert the money \n");
+                        scanf("%d", &moneyInserted);
+                        money += moneyInserted;
+                        event = CHECK_MONEY;
+                    break;
+                    
                     case CHECK_MONEY:
-                         if(money < n){
-                             event = NOT_ENOUGH_MONEY;
-                         }
-                         else{
+                        if(money < n){
+                            event = NOT_ENOUGH_MONEY;
+                        }
+                        else{
                              event = ENOUGH_MONEY;
-                         }
+                        }
                     break;
                     
                     case NOT_ENOUGH_MONEY:
-                         printf("Not enough money\n");
+                        printf("Not enough money\n");
+                        event = ASK_FOR_MONEY;
                     break;
                     
                     case ENOUGH_MONEY:
-                         if(money> price){
+                        if(money> price){
                             state = DISPENCE_CHANGE; 
-                         }
-                         else{
+                        }
+                        else{
                              state = DISPENCE_COKE;
-                         }
+                        }
                     break;
                 }
             break;
+            
             case DISPENCE_COKE:
                 printf("Here is your coke\n");
                 state = EXIT_LOOP;
             break;
+            
             case DISPENCE_CHANGE:
                 printf("Your change is %d\n", money - price);
                 state = DISPENCE_COKE;
             break;
+            
             case EXIT_LOOP:
                 printf("Enjoy!\n");
                 money = 0;
-                moneyInserted = 0;
                 y --;
-                state = ASK_FOR_MONEY;
+                state = START;
+                event = ASK_FOR_MONEY;
             break;
-            
         }
     }
 }
