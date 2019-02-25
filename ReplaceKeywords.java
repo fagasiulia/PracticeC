97ef6979cf75ae05643c384a84c6bfe0b6f188b2
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-02-25 20:34:30 +0200

    Update ReplaceKeywords.java

--- ReplaceKeywords.java
+++ ReplaceKeywords.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
 import java.util.*;
 import java.io.*;
+import java.text.SimpleDateFormat;
 
-public class ReplaceKeywords
+public class ReadingFiles
 {
 	public static void main(String[] args) {
 	    
 		Scanner sourceFile;
-		Scanner targetFile;
+		PrintWriter targetFile;
 		boolean sourceFileExists = false;
 		boolean targetFileExists = false;
 		String oldCity = "Höhenkirchen";
@@ -16,7 +18,7 @@ public class ReplaceKeywords
 		
 		//Check if the source file exists
 		try{
-		  sourceFile = new Scanner(new File("source.txt"));  
+		  sourceFile = new Scanner(new File("sourceFile.txt"));  
 		  sourceFileExists = true;
 		}
 		catch(Exception e){
@@ -25,7 +27,7 @@ public class ReplaceKeywords
 		
 		//Check if the target file exists
 		try{
-		  targetFile = new Scanner(new File("target.txt"));
+		  targetFile = new PrintWriter(new File("targetFile.txt"));
 		  targetFileExists = true;
 		}
 		catch(Exception e){
@@ -35,15 +37,17 @@ public class ReplaceKeywords
 		//Create input and output files
 		if (sourceFileExists && targetFileExists){
 		  try(
-		    Scanner input = new Scanner(sourceFile);
-		    PrintWriter output = new PrintWriter(targetFile);){
+		    Scanner input = sourceFile;
+		    PrintWriter output = targetFile;){
 		        
 		        while(input.hasNext()){
 		            String s1 = input.nextLine();
 		            String s2 = s1.replaceAll(oldCity, newCity);
-		            s2 = replaceAll (oldDate, newDate);
+		            s2 = s1.replaceAll (oldDate, newDate);
 		            output.println(s2);
 		        }
+		    }catch(Exception e) {
+		    	System.out.println("Failed to complete");
 		    }		    
 		}
 	}
