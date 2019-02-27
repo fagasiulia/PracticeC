e7e76b5fae4d6cb12249e768c2fe20bc67dae8eb
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-02-27 16:17:24 +0200

    Update DateRegex.java

--- DateRegex.java
+++ DateRegex.java
@@ -4,10 +4,13 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		
 		String testString = "This is a test 2012-11-27";
-		String pattern = "(\\s*)([0-9]{4}-[0-1][0-9]-[1-3][0-9])(\\s*)"; //Date pattern
-		String cityPattern = "";
+		
+		//Patterns 
+		String pattern = "(\\s*)([0-9]{4}-[0-1][0-9]-[1-3][0-9])(\\s*)"; 
+		String cityPattern = "(\\s*)" + (city) + "(\\s*)";
+		
+		//Implement here
 		String test = replaceKeywords (testString, pattern, " Date");
 		System.out.println(test);
 		
@@ -15,14 +18,38 @@ public class Main
 	public static String replaceKeywords(String stringYouWantToCheck, String pattern, String replacement){
 	    Pattern pw = Pattern.compile(pattern);
 	    Matcher mt = pw.matcher(stringYouWantToCheck);
-	   // int patternLen = mt.group(0).length();
-	    //int replacementLen = replacement.length();
 	    
-	    System.out.println(mt.group(0).length());
-	    
+	    //If a match if found do this... 
 	    if(mt.find()){
 	        stringYouWantToCheck = stringYouWantToCheck.replaceAll(pattern, replacement);
+	        System.out.println(mt.group(0));
+	        int patternLenGroup0 = mt.group(0).length();
+	        System.out.println(mt.group(1));
+	        int patternLenGroup1 = mt.group(1).length();
+	        System.out.println(mt.group(2));
+	        int patternLenGroup2 = mt.group(2).length();
+	        
+	        StringBuffer newString = "";
+	        int length = patternLenGroup1 - (replacement.length());
+            if (length > 0){
+                newString = alignedText(length);
+            }
+            else if (length < 0){
+                int newLength = mt.length() - (patternLenGroup0 + replacement.length())
+                newString = alignedText (patternLenGroup0);
+            }
 	    }
+	     
+	    //Return the new string
 	    return stringYouWantToCheck;
 	}
+	
+	//Method for aligning the text
+	public static StringBuffer alignedText(int length){
+	    StringBuffer outputBuffer = new StringBuffer(length);
+	    for(int i = 0; i < length; i++){
+	        outputBuffer.append(" ");
+	    }
+	    return outputBuffer;
+    }
 }
