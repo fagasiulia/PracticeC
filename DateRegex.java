a84f5322425a8212d8d175557bc84af8fd01c541
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-02-27 15:25:13 +0200

    Update DateRegex.java

--- DateRegex.java
+++ DateRegex.java
@@ -6,14 +6,19 @@ public class Main
 	public static void main(String[] args) {
 		
 		String testString = "This is a test 2012-11-27";
-		String pattern = "((?:(?:1[6-9]|2[0-9])\\d{2})(-)(?:(?:(?:0[13578]|1[02])(-)31)|((0[1,3-9]|1[0-2])(-)(29|30))))|(?:(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(-)02(-)29)|(?:(?:1[6-9]|2[0-9])\\d{2})(-)(?:(?:0[1-9])|(?:1[0-2]))(-)(?:0[1-9]|1\\d|2[0-8])";
-		String test = replaceKeywords (testString, pattern, "Date");
+		String pattern = "(\\s*)([0-9]{4}-[0-1][0-9]-[1-3][0-9])(\\s*)"; //Date pattern
+		String cityPattern = "";
+		String test = replaceKeywords (testString, pattern, " Date");
 		System.out.println(test);
 		
 	}
 	public static String replaceKeywords(String stringYouWantToCheck, String pattern, String replacement){
 	    Pattern pw = Pattern.compile(pattern);
 	    Matcher mt = pw.matcher(stringYouWantToCheck);
+	   // int patternLen = mt.group(0).length();
+	    //int replacementLen = replacement.length();
+	    
+	    System.out.println(mt.group(0).length());
 	    
 	    if(mt.find()){
 	        stringYouWantToCheck = stringYouWantToCheck.replaceAll(pattern, replacement);
