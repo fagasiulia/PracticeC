62e287a45554040958ec6e388030991e302a5a1d
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-02-27 21:25:02 +0200

    Update DateRegex.java

--- DateRegex.java
+++ DateRegex.java
@@ -1,3 +1,5 @@
+package practice;
+
 import java.io.*;
 import java.util.regex.*;
 import java.util.*;
@@ -8,6 +10,7 @@ public class Main
 		
 		//Patterns 
 		String pattern = "(\\s*)([0-9]{4}-[0-1][0-9]-[1-3][0-9])(\\s*)"; 
+		String city = "Timisoara";
 		String cityPattern = "(\\s*)" + (city) + "(\\s*)";
 		
 		//Implement here
@@ -21,23 +24,32 @@ public class Main
 	    
 	    //If a match if found do this... 
 	    if(mt.find()){
-	        stringYouWantToCheck = stringYouWantToCheck.replaceAll(pattern, replacement);
-	        System.out.println(mt.group(0));
-	        int patternLenGroup0 = mt.group(0).length();
-	        System.out.println(mt.group(1));
-	        int patternLenGroup1 = mt.group(1).length();
-	        System.out.println(mt.group(2));
-	        int patternLenGroup2 = mt.group(2).length();
+	    	//Get the length of each group
+	    	
+	    	// group(0) is for the full length
+	        int patternLength = mt.group(0).length();
 	        
-	        StringBuffer newString = "";
-	        int length = patternLenGroup1 - (replacement.length());
+	        // group(1) is for the space in front of the keyword
+	        int firstGroupLength = mt.group(1).length();
+	        
+	        // group(2) is for the keyword
+	        int secondGroupLength = mt.group(2).length();
+	     
+	        // group(3) is for the space after the keyword
+	        int lastGroupLength = mt.group(3).length();
+	        
+	        //Create a StringBuffer to use for the space in front and after the keyword
+	        StringBuffer newString = null;
+	        int length = secondGroupLength - (replacement.length());
             if (length > 0){
                 newString = alignedText(length);
             }
             else if (length < 0){
-                int newLength = mt.length() - (patternLenGroup0 + replacement.length())
-                newString = alignedText (patternLenGroup0);
+                int newLength = patternLength - (firstGroupLength + replacement.length());
+                newString = alignedText (patternLength);
             }
+            replacement = alignedText(mt.group(1).length()) + replacement + newString;
+            stringYouWantToCheck = stringYouWantToCheck.replaceAll(pattern, replacement);
 	    }
 	     
 	    //Return the new string
