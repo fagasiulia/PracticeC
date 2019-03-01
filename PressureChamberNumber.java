3511f2e34dea3e0c955dcb2e6912026890696d19
fagasiulia <47377829+fagasiulia@users.noreply.github.com>
2019-03-01 14:41:46 +0200

    Update PressureChamberNumber.java

--- PressureChamberNumber.java
+++ PressureChamberNumber.java
@@ -1,13 +1,42 @@
-
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.regex.*;
+import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-	    String word = "Pressure_Chamber_";
-	    String number = "11";
-	    String finalString = pressureChamberNumberReplacement(word, number);
-	    
-		System.out.println(finalString);
+		String toCheck = "Pressure_Chamber_15";
+		String pattern = "(Pressure_Chamber_)(\\d*)";
+		String n = replaceKeywords(toCheck, pattern);
+		System.out.println(n);
 	}
+	
+	//Method for aligning the text
+	public static StringBuffer alignedText(int length){
+	    StringBuffer outputBuffer = new StringBuffer(length);
+	    for(int i = 0; i < length; i++){
+	        outputBuffer.append(" ");
+	    }
+	    return outputBuffer;
+    }
+	
+    //Change Pressure Chamber's Number
+	public static String replaceKeywords(String stringYouWantToCheck, String pattern){
+	    Pattern pw = Pattern.compile(pattern);
+	    Matcher mt = pw.matcher(stringYouWantToCheck);
+	    String string = stringYouWantToCheck;
+	    //If a match if found do this... 
+	    if(mt.find()){
+	        String word = mt.group(1);
+	        String number = mt.group(2);
+            String replacement = pressureChamberNumberReplacement(word, number);
+            string = stringYouWantToCheck.replaceAll(pattern, replacement);
+	   }
+	     
+	    //Return the new string
+	    return string;
+	}
+	
 	public static String pressureChamberNumberReplacement(String word, String number){
 	    int repLength = number.length();
 	    int decimalNumber = Integer.parseInt(number) - 1;
@@ -21,12 +50,4 @@ public class Main
 	    }
 	    return word;
 	}
-	//Method for aligning the text
-	public static StringBuffer alignedText(int length){
-	    StringBuffer outputBuffer = new StringBuffer(length);
-	    for(int i = 0; i < length; i++){
-	        outputBuffer.append(" ");
-	    }
-	    return outputBuffer;
-    }
 }
